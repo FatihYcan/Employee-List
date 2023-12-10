bc703411709a9bf1f4e6b5b84cff44799158e9fb
FATİH <f.yakutcan.32@gmail.com>
2023-12-10 18:53:58 +0300

    finished

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import List from "./components/List";
+import { useState } from "react";
+import data from "./helper/data";
+
+function App() {
+  const [index, setIndex] = useState(0);
+
+  const checkNumber = (number) => {
+    if (number > data.length - 5) {
+      return 0;
+    }
+    if (number < 0) {
+      return data.length - 5;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 5;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 5;
+      return checkNumber(newIndex);
+    });
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>Employee List</h3>
+        <h5>
+          (Employees {index + 1} to {index + 5})
+        </h5>
+        <List data={data.slice(index, index + 5)} />
+        <div className="btns">
+          <button onClick={prevPerson}>Prev</button>
+          <button onClick={nextPerson}>Next</button>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const List = ({ data }) => {
+  return (
+    <>
+      {data.map(({ id, name, age, image, email }) => (
+        <article className="person">
+          <img src={image} alt={"name"} />
+          <div>
+            <h4>{name}</h4>
+            <p>{email}</p>
+            <p>{age} years</p>
+          </div>
+        </article>
+      ))}
+    </>
+  );
+};
+
+export default List;
